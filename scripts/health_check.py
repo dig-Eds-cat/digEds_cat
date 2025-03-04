7e1c0ec1efa6445c63b18fc0698bda1f4463111b
csae8092 <p.andorfer@gmail.com>
2025-03-04 11:56:54 +0100

    feat: reconsile with health_check result

---
+++ scripts/health_check.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = "digEds_cat.csv"
+source_df = pd.read_csv(
+    "https://raw.githubusercontent.com/dig-Eds-cat/qod/refs/heads/main/result.csv"
+)
+target_df = pd.read(data)
+target_df.loc[
+    target_df["URL"].isin(source_df[source_df["status"] != 200]["URL"]), "gone"
+] = "yes"
+
+target_df.to_csv(data, index=False)
