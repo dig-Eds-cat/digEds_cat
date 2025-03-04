e4794b0990aa3c42cd809a57dee17cdfffd179cc
csae8092 <p.andorfer@gmail.com>
2025-03-04 11:58:42 +0100

    feat: run health check via gh-action

--- scripts/health_check.py
+++ scripts/health_check.py
@@ -4,7 +4,7 @@ data = "digEds_cat.csv"
 source_df = pd.read_csv(
     "https://raw.githubusercontent.com/dig-Eds-cat/qod/refs/heads/main/result.csv"
 )
-target_df = pd.read(data)
+target_df = pd.read_csv(data)
 target_df.loc[
     target_df["URL"].isin(source_df[source_df["status"] != 200]["URL"]), "gone"
 ] = "yes"
